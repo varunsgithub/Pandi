cb48da14984f660286d1bf8b5ef1d5948f764221
varunsingh <varsingh@seas.upenn.edu>
2025-06-13 09:11:44 -0400

    Updated the Ast generator

---
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,21 @@
+package com.craftinginterpreters.pandi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Arrays;
+
+// This class is used for generating the Abstract Syntax Tree
+public class GenerateAst {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output_directory>");
+            System.exit(64);
+        }
+
+
+
+    }
+
+}
